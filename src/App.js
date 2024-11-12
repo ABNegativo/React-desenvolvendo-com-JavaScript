d1aaf12d48c15077ec9e7db6341d13a354864db9
Higor Bernardes <zdtshark@gmail.com>
2024-11-12 09:58:16 -0400

    enviando arquivos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Formulario />
+
+      <Time nome="Programação" />
+      <Time nome="Frontend" />
+      <Time nome="Data Science" />
     </div>
   );
 }
